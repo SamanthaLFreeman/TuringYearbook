d8f149c4aef8f2861eb9bc11ae3addafba9c0f51
Alan Turing <freemasl.sf@gmail.com>
2019-08-12 17:37:29 -0600

    Create Cohort component and Person component to display instructors and students

--- src/components/Cohort.js
+++ src/components/Cohort.js
@@ -2,8 +2,15 @@ import React from 'react';
 import Person from './Person';
 import './Cohort.css';
 
-const Cohort = () => {
-  
+const Cohort = (props) => {
+  return (
+    <main className="cohortContainer">
+      {props.staff.map(person => <Person individual={person} key={person.id}/>
+      )}
+      {props.students.map(person => <Person individual={person} key={person.id} />
+      )}
+    </main>
+  )
 }
 
 export default Cohort;
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Person.css';
 
-const Person = () => {
-
+const Person = ({individual}) => {
+  return (
+    <section className="personContainer" key={individual.id}>
+      <img className="image" src={individual.photo} alt={individual.name} />
+      <p className="name">{individual.name}</p>
+      <p className="quote">"{individual.quote}"</p>
+      <p className="superlative">{individual.superlative}</p>
+    </section>
+  )
 }
 
 export default Person;
