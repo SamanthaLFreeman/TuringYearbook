2561568dc7f70d1b558d09a98af50b068b861a7e
Alan Turing <freemasl.sf@gmail.com>
2019-08-12 18:02:51 -0600

    Add functionality to set state based on what the user has entered

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -4,17 +4,51 @@ class AddStudent extends Component {
   constructor() {
     super();
     this.state = {
-
+      name: "",
+      quote: "",
+      superlative: ""
     }
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      name: "",
+      quote: "",
+      superlative: ""
+    })
   }
 
   render() {
     return (
       <form>
-        <input type="text" placeholder="name" name="name"></input>
-        <input type="text" placeholder="quote" name="quote"></input>
-        <input type="text" placeholder="superlative" name="superlative"></input>
-        <button>Submit</button>
+        <input 
+        type="text" 
+        placeholder="name" 
+        name="name" 
+        value={this.state.name} 
+        onChange={this.handleChange}
+></input>
+        <input 
+        type="text" 
+        placeholder="quote" 
+        name="quote" 
+        value={this.state.quote} 
+        onChange={this.handleChange}
+></input>
+        <input 
+        type="text" 
+        placeholder="superlative" 
+        name="superlative"
+        value={this.state.superlative}
+        onChange={this.handleChange}
+></input>
+        <button
+        onClick={(e) => this.handleSubmit(e)}>Submit</button>
       </form>
     )
   }
