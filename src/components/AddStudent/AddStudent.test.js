6748868714af5741619a9ef5cdfec4118d575ff5
Alan Turing <freemasl.sf@gmail.com>
2019-08-15 20:30:35 -0600

    Crete initial snapshot tests for all components

--- src/components/AddStudent/AddStudent.test.js
+++ src/components/AddStudent/AddStudent.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import AddStudent from './AddStudent';
 
-// describe('AddStudent', () => {
-//   it('should pass', () => {
-//     expect.
-//   })
-// })
+describe('AddStudent', () => {
+  it('should match the snapshot with all data passed in correctly', () => {
+    const wrapper = shallow(<AddStudent addStudent={jest.fn()}/>);
+
+    expect(wrapper).toMatchSnapshot();
+  })
+})
