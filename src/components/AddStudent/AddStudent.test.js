fa9bbe4a8f2e12a189f8fb6a5d29beb7646f9c5e
Alan Turing <freemasl.sf@gmail.com>
2019-08-17 14:35:46 -0600

    Add test to check the state has been updated when the input is updated

--- src/components/AddStudent/AddStudent.test.js
+++ src/components/AddStudent/AddStudent.test.js
@@ -9,4 +9,19 @@ describe('AddStudent', () => {
     expect(wrapper).toMatchSnapshot();
   })
 
+  it('should update state when the input is changed', () => {
+    const wrapper = shallow(<AddStudent addStudent={jest.fn()} />);
+    const input = wrapper.find('.name-input');
+    const expected = {
+      photo: 'https://placekitten.com/200/300',
+      name: 'Sam',
+      quote: '',
+      superlative: ''
+    };
+
+    input.simulate('change', { target: { name: 'name', value: 'Sam' } });
+
+    expect(wrapper.state()).toEqual(expected);
+  })
+
 })
