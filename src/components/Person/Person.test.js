758d6fa51652edcf5eaece4f661f33a4daed96e9
Alan Turing <freemasl.sf@gmail.com>
2019-08-17 12:04:44 -0600

    Fix snapshot tests and add additional snapshot test for staff

--- src/components/Person/Person.test.js
+++ src/components/Person/Person.test.js
@@ -3,14 +3,26 @@ import { shallow } from 'enzyme';
 import Person from './Person';
 
 describe('Person', () => {
-  it('should match the snapshot with all data passed in correctly', () => {
+  it('should match the snapshot with all data passed in correctly if they are a student', () => {
     const person = { 
       id: 1, 
       name: 'Sam', 
       quote: 'Huh?', superlative: 'Most likely to', 
       studentCheck: 'student'
     }
-    const wrapper = shallow(<Person individual={person}  removeStudent={jest.fn()} />)
+    const wrapper = shallow(<Person individual={person} removeStudent={jest.fn()} studentCheck={'student'}/>)
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should match the snapshot with all data passed in correctly if they are staff', () => {
+    const person = {
+      id: 1,
+      name: 'Robbie',
+      quote: 'pants...', superlative: 'Most likely to',
+      studentCheck: 'staff'
+    }
+    const wrapper = shallow(<Person individual={person} removeStudent={jest.fn()} studentCheck={'staff'} />)
 
     expect(wrapper).toMatchSnapshot()
   })
