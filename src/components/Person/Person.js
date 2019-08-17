6e733bb6a691ad3afcfd0c092469ef9729c0bc71
Alan Turing <freemasl.sf@gmail.com>
2019-08-17 17:06:28 -0600

    Add filler text for quotes

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -9,7 +9,7 @@ const Person = ({individual, removeStudent, studentCheck}) => {
     <section className="personContainer" key={individual.id}>
       <img className="image" src={individual.photo} alt={individual.name} />
       <h3 className="name" contentEditable={editableCheck} suppressContentEditableWarning={true}>{individual.name}</h3>
-      <p className="quote" contentEditable={editableCheck} suppressContentEditableWarning={true}>{individual.quote}</p>
+      <p className="quote" contentEditable={editableCheck} suppressContentEditableWarning={true}>"{individual.quote}"</p>
       <p className="superlative" contentEditable={editableCheck} suppressContentEditableWarning={true}>{individual.superlative}</p>
       {button}
     </section>
