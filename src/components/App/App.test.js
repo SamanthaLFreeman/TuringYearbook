597af9c57aafd5f897679c33780799201718cf60
Alan Turing <freemasl.sf@gmail.com>
2019-08-17 11:38:45 -0600

    Test the addStudent method in App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+
+  it('should update state with a new student', () => {
+    const wrapper = shallow(<App />);
+    const mockStudent = {
+      id: 1,
+      name: 'Sam',
+      quote: 'hmmm',
+      superlative: 'Most likely to'
+    };
+    const mockStudent2 = {
+      id: 2,
+      name: 'Jessie',
+      quote: 'ummm',
+      superlative: 'Most likely to'
+    }
+    const expected = [mockStudent, mockStudent2];
+
+    wrapper.setState({students: [mockStudent]});
+    wrapper.instance().addStudent(mockStudent2);
+
+    expect(wrapper.state('students')).toEqual(expected)
+  })
+
+  
+
+})
--- src/components/Person/Person.test.js
+++ src/components/Person/Person.test.js
@@ -14,4 +14,5 @@ describe('Person', () => {
 
     expect(wrapper).toMatchSnapshot()
   })
+
 })
