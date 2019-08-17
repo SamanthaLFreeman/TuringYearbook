bc85e577f829b69d6e19690de32c3568944f0349
Alan Turing <freemasl.sf@gmail.com>
2019-08-17 14:25:49 -0600

    Add example objects in the Cohort component test

--- src/components/Cohort/Cohort.test.js
+++ src/components/Cohort/Cohort.test.js
@@ -4,7 +4,17 @@ import Cohort from './Cohort';
 
 describe('Cohort', () => {
   it('should match the snapshot with all data passed in correctly', () => {
-    const wrapper = shallow(<Cohort staff={[]} students={[]} removeStudent={jest.fn()} />);
+    const wrapper = shallow(<Cohort staff={[{
+      id: 1,
+      name: 'Robbie',
+      quote: 'pants...', superlative: 'Most likely to',
+      studentCheck: 'staff'
+    }]} students={[{
+      id: 1,
+      name: 'Sam',
+      quote: 'Huh?', superlative: 'Most likely to',
+      studentCheck: 'student'
+    }]} removeStudent={jest.fn()} />);
 
     expect(wrapper).toMatchSnapshot();
   })
